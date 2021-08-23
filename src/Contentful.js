e8baf3cfb83a6980cb70a800a5089072aa92878c
hamza-che <chenguitiha@gmail.com>
2021-08-23 12:58:00 +0100

    env variables file added

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: "vfnvl04ja70g",
-  accessToken: "OfX7JEJ61LM6sjck8FdlaxGq1PSOPM75xvkrwHPcPSY",
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
